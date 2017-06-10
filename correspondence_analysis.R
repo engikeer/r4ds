8c45dd82d00c4f7cc4c6b279aeb5dd601633a83d
engikeer <engikeer@163.com>
2017-06-10 23:41:18 +0800

    add

--- correspondence_analysis.R
+++ correspondence_analysis.R
@@ -1,1 +1,10 @@
 library(tidyverse)
+
+# ?tidy data
+scores <- tibble(
+    ability = rep(c("Pure-Ch", "Semi-Ch", "Pure-En"), 4),
+    student = c(rep("A", 3), rep("B", 3), rep("C", 3), rep("D", 3)),
+    score = c(47, 22, 10, 31, 32, 11, 2, 21, 25, 1, 10, 20)
+)
+
+tb <- spread(scores, student, score)
